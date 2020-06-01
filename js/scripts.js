9a0c1c41103610e6ff18be2bd75783925ed519b1
Casey The Great <casey@caseypitman.com>
2020-06-01 13:29:08 -0500

    open exchnge

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,5 @@
 //Call for currency types
 
-let typeEndpoint ="list";
-let liveEndpoint="live";
-let accessKey="a18afaa369c7dccb20013b484bf05bd3";
 let inputAmount=0;
 let inputType="" //may not need - set to default USD
 let outputType="";
@@ -10,22 +7,21 @@ let outputAmount=0;
 
 //Call for list of available currencies.
 $(document).ready(function(){
-    $.ajax({
-        url:"http://api.currencyLayer.com/" + typeEndpoint + "?access_key=" + accessKey,
-        dataType:"jsonp",
-        success: function(json){
-            for (let property in json.currencies){
-                let optionVal = property; //sets the value for the option
-                let optionDis = json.currencies[property]; //sets display text for the option
-                let node = document.createElement("option"); //create new option
-                node.setAttribute("value", optionVal); //sets the value assigned to the option
-                let textnode=document.createTextNode(optionDis);//sets display text for the option
-                node.appendChild(textnode); //attach text to the option
-                document.getElementById("output-type").appendChild(node); //attaches to the DOM (input selector)
-            }
+    $.get('https://openexchangerates.org/api/currencies.json', function(data) {
+        //console.log(data);
+        for (let property in data){
+            //console.log(data[property]);
+            let optionVal = property; //sets the value for the option
+            let optionDis = data[property]; //sets display text for the option
+            let node = document.createElement("option"); //create new option
+            node.setAttribute("value", optionVal); //sets the value assigned to the option
+            let textnode=document.createTextNode(optionDis);//sets display text for the option
+            node.appendChild(textnode); //attach text to the option
+            document.getElementById("output-type").appendChild(node); //attaches to the DOM (input selector)
         }
-    })
+    });
 });
+
 
 function convert(){
     inputAmount=$("#input-amount").val();
@@ -36,16 +32,12 @@ function convert(){
     if (outputType===""){
         alert("Please select a type of currency for your conversion.")
     }
-    //make the ajax call
-    $.ajax({
-        url: 'http://api.currencylayer.com/'+ liveEndpoint + "?access_key=" + accessKey +"&currencies="+outputType + "&format=1",
-        dataType:'jsonp',
-        success: function(json){
-        let conversionVal = (json.quotes["USD"+outputType]); //determine the relative value of the currency to the US dollar
-        let resultVal = inputAmount * conversionVal;
-        $("#output-amount").val(resultVal);
-        }
-    })
+
+    $.get('https://openexchangerates.org/api/latest.json', {app_id: '6088cc29466f4a299e2690ae0e2f9f9e'}, function(data) {
+        let conversionVal = (data.rates[outputType]); //determine the relative value of the currency to the US dollar
+        let resultVal = (inputAmount * conversionVal).toFixed(2); //Do the conversion
+        $("#output-amount").val(resultVal); // Display result
+    });
 }
 
 $("#convert-btn").on("click", convert);
