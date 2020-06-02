82f847d1c830d9c20047b68045bb7491a5d72c99
Casey The Great <casey@caseypitman.com>
2020-06-01 20:09:22 -0500

    tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,28 @@
 
 $(document).ready(function(){
-
+    //Converts from one currency to another
     function convert(){
-        let inputCurrency=$("#input-currency").val();
+        let inputCurrency=$("#input-currency").val(); 
         let outputCurrency=$("#output-currency").val();
         let inputAmount=$("#input-amount").val();
         let outputText=$("#output-currency option:selected").text();
-
+        //Call for current exchange rates
         $.getJSON("https://api.ratesapi.io/api/latest?base=" + inputCurrency + "&symbols=" + outputCurrency, function(data){
-            if (inputCurrency===""){
+            if (inputCurrency===""){ //User doesn't choose input currency
                 alert("Please choose the type of currency you wish to convert from.");
             }
-            if(inputAmount===""){
+            if(inputAmount===""){ //User doesn't input an amount
                 alert("Plese enter an amount to convert.");
             }
-            if(outputCurrency===""){
+            if(outputCurrency===""){ //User doesn't choose ouput currency
                 alert("Please enter the type of currency you with to convert to.");
             }
+            //Does the math for the conversion. 
             let results= (inputAmount * data.rates[outputCurrency]).toFixed(2);
-            console.log(results + " " + outputText);
+            //Displays the result
             $("#output-amount").val(results + " " + outputText);       
         })
     }
+    //Activates the conversion
     $("#convert-btn").on("click", convert);
 });
