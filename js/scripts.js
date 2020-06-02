c057e1ce364fe9e8c6429157426ccb93e62bcb4f
Casey The Great <casey@caseypitman.com>
2020-06-01 19:01:12 -0500

    New background

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,26 @@
-//Call for currency types
 
-let inputAmount=0;
-let inputType="" //may not need - set to default USD
-let outputType="";
-let outputAmount=0;
-
-//Call for list of available currencies.
 $(document).ready(function(){
-    $.get('https://openexchangerates.org/api/currencies.json', function(data) {
-        //console.log(data);
-        for (let property in data){
-            //console.log(data[property]);
-            let optionVal = property; //sets the value for the option
-            let optionDis = data[property]; //sets display text for the option
-            let node = document.createElement("option"); //create new option
-            node.setAttribute("value", optionVal); //sets the value assigned to the option
-            let textnode=document.createTextNode(optionDis);//sets display text for the option
-            node.appendChild(textnode); //attach text to the option
-            document.getElementById("output-type").appendChild(node); //attaches to the DOM (input selector)
-        }
-    });
+
+    function convert(){
+        let inputCurrency=$("#input-currency").val();
+        let outputCurrency=$("#output-currency").val();
+        let inputAmount=$("#input-amount").val();
+        let outputText=$("#output-currency option:selected").text();
+
+        $.getJSON("https://api.ratesapi.io/api/latest?base=" + inputCurrency + "&symbols=" + outputCurrency, function(data){
+            if (inputCurrency===""){
+                alert("Please choose the type of currency you wish to convert from.");
+            }
+            if(inputAmount===""){
+                alert("Plese enter an amount to convert.");
+            }
+            if(outputCurrency===""){
+                alert("Please enter the type of currency you with to convert to.");
+            }
+            let results= (inputAmount * data.rates[outputCurrency]).toFixed(2);
+            console.log(results + " " + outputText);
+            $("#output-amount").val(results + " " + outputText);       
+        })
+    }
+    $("#convert-btn").on("click", convert);
 });
-
-
-function convert(){
-    inputAmount=$("#input-amount").val();
-    if (inputAmount===""){   //ensures an amount is entered
-        alert("Please enter a dollar amount.")
-    }
-    outputType = $("#output-type").val()
-    if (outputType===""){
-        alert("Please select a type of currency for your conversion.")
-    }
-
-    $.get('https://openexchangerates.org/api/latest.json', {app_id: '6088cc29466f4a299e2690ae0e2f9f9e'}, function(data) {
-        let conversionVal = (data.rates[outputType]); //determine the relative value of the currency to the US dollar
-        let resultVal = (inputAmount * conversionVal).toFixed(2); //Do the conversion
-        $("#output-amount").val(resultVal); // Display result
-    });
-}
-
-$("#convert-btn").on("click", convert);
