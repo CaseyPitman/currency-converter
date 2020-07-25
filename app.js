80b4684c7459d1e98287752a31963a9b63a7e9a9
Casey The Great <casey@caseypitman.com>
2020-07-25 12:53:08 -0500

    Built app structure

--- app.js
+++ app.js
@@ -1,15 +1,94 @@
 //Currency Converter App
 
 
+/* ********** Data ********** */
+
+const dataControl = (() => {
+
+
+    //Retrieve current exchange rates async/await try/catch
+    //     "https://api.ratesapi.io/api/latest?base=" + inputCurrency + "&symbols=" + outputCurrency
+
+    //Make the conversion
+
+    //Return results
+
+})();
+
+/* ********** UI ********** */
+
+const uiControl = (() => {
+    //DOMstrings
+    const elements = {
+        inputAmount: document.getElementById('input-amount'),
+        inputCurrency: document.getElementById('input-currency'),
+        outputCurrency: document.getElementById('output-currency'),
+        convertBtn: document.getElementById('convert-btn'),
+        output: document.getElementById('output')
+    }
+
+    return {  
+        //Get inputs
+
+
+        //Display results
+
+
+        //Hide results
+
+
+        //Return Elements
+        getElements: () => {
+            return elements;
+        }
+
+    }
+    
+
+})();
+
+
+/* ********** Controller ********** */
+
+const controller = ((data, ui) => {
+
+    // Get DOMstrings
+    const el = ui.getElements();
+    console.log(el);
+
+
+    //Set up event listeners
+
+
+    //Retrieve inputs
+
+
+    //Retrieve current exchange rates
+
+
+    //Convert the input to the desired currency
+
+
+    //Display the results
+
+
+    //return init function
+        //Hide results
+
+
+})(dataControl, uiControl);
+
+/* Initialize App */
+
+//init();
+
+/* 
+************************TESTING AREA***********************
+ */
 
 
 
-
-
-
-/* $(document).ready(function(){
-
-    //Converts from one currency to another
+/*
     function convert(){
         let inputCurrency=$("#input-currency").val(); 
         let outputCurrency=$("#output-currency").val();
@@ -32,6 +111,5 @@
             $("#output-amount").val(results + " " + outputText);       
         })
     }
-    //Activates the conversion
-    $("#convert-btn").on("click", convert);
+
 }); */
